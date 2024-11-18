ada1548aebcba1d73957622252bbfc79c06d3759
sanjeevshah1 <sahsanjeev42@gmail.com>
2024-11-18 22:52:11 +0545

    moved support functions to utils

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,17 @@ import bencodec from 'bencodec';
 import { createHash } from 'crypto';
 import { decodeBencode } from './Bencode/decodeBencode';
 import { TorrentFile, Info } from './types';
-import { extractPieceHashes, urlEncodeHash, fetchTracker } from './utils';
+import { extractPieceHashes,
+    urlEncodeHash,
+    fetchTracker,
+    sendKeepAlive,
+    connectToPeer,
+    sendHandshake,
+    sendInterested,
+    waitForUnchoke,
+    uint32ToBuffer,
+    waitForMessage,
+} from './utils';
 
 const color = require('colors-cli/toxic')
 const loading =  require('loading-cli');
@@ -163,18 +173,6 @@ async function handshake(hash: Buffer, peerId: string, portNumber: number) {
 
 const DEFAULT_CHUNK_SIZE = 2 ** 14;
 
-const MessageTypes = [
-  "choke",
-  "unchoke",
-  "interested",
-  "not interested",
-  "have",
-  "bitfield",
-  "request",
-  "piece",
-  "cancel",
-  "handshake",
-] as const;
 
 
 async function downloadFile(
@@ -283,56 +281,14 @@ async function downloadFile(
         }
     }
 }
-function sendKeepAlive(socket: net.Socket) {
 
-    const keepAlive = Buffer.alloc(4);
-    socket.write(keepAlive);
-    // console.log("Sent keep-alive message");
-}
 
-function connectToPeer(
-    socket: net.Socket,
-    peerId: string,
-    portNumber: number
-): Promise<void> {
 
-    return new Promise((resolve, reject) => {
-        socket.connect(portNumber, peerId, () => {
-            // console.log(`Connected to peer ${peerId}:${portNumber}`);
-            resolve();
-        });
-        socket.on('error', reject);
-    });
-}
 
-function sendHandshake(socket: net.Socket, hash: Buffer): Promise<Buffer> {
 
-    const handshake = Buffer.concat([
-        Buffer.from([19]),
-        Buffer.from("BitTorrent protocol"),
-        Buffer.alloc(8),
-        hash,
-        Buffer.from("00112233445566778899"),
-    ]);
 
-    socket.write(handshake);
 
-    return waitForMessage(socket, "handshake");
 
-}
-
-function sendInterested(socket: net.Socket): void {
-    socket.write(Buffer.from([0, 0, 0, 1, 2]));
-}
-
-function waitForUnchoke(socket: net.Socket): Promise<void> {
-
-    return new Promise<void>(async (resolve) => {
-        const message = await waitForMessage(socket, "unchoke");
-        // console.log("Received unchoke message", message);
-        resolve();
-    });
-}
 
 async function downloadPiece(
     socket: net.Socket,
@@ -412,50 +368,7 @@ function requestPiece(
             });
     });
 }
-function waitForMessage(socket: net.Socket, expectedType: string): Promise<Buffer> {
 
-    return new Promise((resolve, reject) => {
-        let buffer = Buffer.alloc(0);
-
-        function handleData(data: Buffer) {
-            buffer = Buffer.concat([buffer, data]);
-            
-            while (buffer.length > 0) {
-
-                if (expectedType === "handshake") {
-                    if (buffer.length < 68) return;
-                    resolve(buffer.subarray(0, 68));
-                    buffer = buffer.subarray(68);
-                    return;
-                }
-
-                if (buffer.length < 4) return;
-                const messageLength = buffer.readUInt32BE(0);
-
-                if (buffer.length < messageLength + 4) return;
-
-                const messageId = buffer[4];
-                const message = buffer.subarray(0, messageLength + 4);
-                buffer = buffer.subarray(messageLength + 4);
-
-                if (MessageTypes[messageId] === expectedType) {
-                    resolve(message);
-                    return;
-                }
-            }
-        }
-
-        socket.on("data", handleData);
-        socket.once("error", reject);
-        socket.once("close", () => reject(new Error("Connection closed")));
-    });
-}
-
-function uint32ToBuffer(num: number): Buffer {
-    const buf = Buffer.alloc(4);
-    buf.writeUInt32BE(num);
-    return buf;
-}
 
 async function handleDownloadPieceCommand() {
 
--- app/utils.ts
+++ app/utils.ts
@@ -1,6 +1,6 @@
 import { DecodedResponse } from "./types";
 import bencodec from 'bencodec';
-
+import * as net from "node:net";
 export function extractPieceHashes(pieces: Buffer): string[] {
     const hashes: string[] = [];
     for (let i = 0; i < pieces.length; i += 20) {
@@ -45,3 +45,113 @@ export const fetchTracker = async (URL: string) : Promise<string[]> => {
     return parsedPeers;
 
 };
+
+const MessageTypes = [
+    "choke",
+    "unchoke",
+    "interested",
+    "not interested",
+    "have",
+    "bitfield",
+    "request",
+    "piece",
+    "cancel",
+    "handshake",
+  ] as const;
+
+  export function sendKeepAlive(socket: net.Socket) {
+
+    const keepAlive = Buffer.alloc(4);
+    socket.write(keepAlive);
+    // console.log("Sent keep-alive message");
+}
+
+export function connectToPeer(
+    socket: net.Socket,
+    peerId: string,
+    portNumber: number
+): Promise<void> {
+
+    return new Promise((resolve, reject) => {
+        socket.connect(portNumber, peerId, () => {
+            // console.log(`Connected to peer ${peerId}:${portNumber}`);
+            resolve();
+        });
+        socket.on('error', reject);
+    });
+}
+
+export function sendHandshake(socket: net.Socket, hash: Buffer): Promise<Buffer> {
+
+    const handshake = Buffer.concat([
+        Buffer.from([19]),
+        Buffer.from("BitTorrent protocol"),
+        Buffer.alloc(8),
+        hash,
+        Buffer.from("00112233445566778899"),
+    ]);
+
+    socket.write(handshake);
+
+    return waitForMessage(socket, "handshake");
+
+}
+
+export function sendInterested(socket: net.Socket): void {
+    socket.write(Buffer.from([0, 0, 0, 1, 2]));
+}
+
+export function waitForUnchoke(socket: net.Socket): Promise<void> {
+
+    return new Promise<void>(async (resolve) => {
+        const message = await waitForMessage(socket, "unchoke");
+        // console.log("Received unchoke message", message);
+        resolve();
+    });
+}
+
+export function uint32ToBuffer(num: number): Buffer {
+    const buf = Buffer.alloc(4);
+    buf.writeUInt32BE(num);
+    return buf;
+}
+
+export function waitForMessage(socket: net.Socket, expectedType: string): Promise<Buffer> {
+
+    return new Promise((resolve, reject) => {
+        let buffer = Buffer.alloc(0);
+
+        function handleData(data: Buffer) {
+            buffer = Buffer.concat([buffer, data]);
+            
+            while (buffer.length > 0) {
+
+                if (expectedType === "handshake") {
+                    if (buffer.length < 68) return;
+                    resolve(buffer.subarray(0, 68));
+                    buffer = buffer.subarray(68);
+                    return;
+                }
+
+                if (buffer.length < 4) return;
+                const messageLength = buffer.readUInt32BE(0);
+
+                if (buffer.length < messageLength + 4) return;
+
+                const messageId = buffer[4];
+                const message = buffer.subarray(0, messageLength + 4);
+                buffer = buffer.subarray(messageLength + 4);
+
+                if (MessageTypes[messageId] === expectedType) {
+                    resolve(message);
+                    return;
+                }
+            }
+        }
+
+        socket.on("data", handleData);
+        socket.once("error", reject);
+        socket.once("close", () => reject(new Error("Connection closed")));
+    });
+}
+
